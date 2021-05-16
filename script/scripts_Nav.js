036772d204277acf193cd987f3acce054f85a4d8
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2021-05-16 16:13:15 +0700

    add comment 5

--- script/scripts_Nav.js
+++ script/scripts_Nav.js
@@ -1,13 +1,13 @@
-$(function(){
+$(function(){ //สร้าง function
 
-    $(".toggle").on("click",function(){
+    $(".toggle").on("click",function(){  //ทำการเข้าถึง class toggole ถ้ามีการ clickให้ทำงานต่อไปนี้ 
 
-        if($(".item").hasClass("active")){
-            $(".item").removeClass("active");
-            $(this).find("a").html("<i class='fas fa-bars'></i>");
-        } else{
-            $(".item").addClass("active");
-            $(this).find("a").html("<i class='fas fa-times'></i>");
+        if($(".item").hasClass("active")){ //เข้าถึง classs item ถ้าหากมี active ให้ทำดังต่อไปนี้
+            $(".item").removeClass("active"); //ทำการเข้าถึง class item ทำการลบ active
+            $(this).find("a").html("<i class='fas fa-bars'></i>");  //ทำการเข้าถึง class toggle ทำการเปลี่ยน tag ใน class toggleดังนี้ <i class='fas fa-bars'></i>
+        } else{ //หากไม่ตรงตามเงื่อนไขให้ทำดังนี้
+            $(".item").addClass("active");//ทำการเข้าถึง class item เพิ่ม active
+            $(this).find("a").html("<i class='fas fa-times'></i>"); //ทำการเข้าถึง class toggle ทำการเปลี่ยน tag ใน class toggleดังนี้ <i class='fas fa-times'></i>
         }
     });
 });
--- script/scripts_game.js
+++ script/scripts_game.js
@@ -1,65 +1,66 @@
-var score = 0,
-    highScore = 0,
-    time = 30,
-    timer;
-var IsPlaying = false;
-var timeBoard = document.getElementById('time'),
-    scoreBoard = document.getElementById('score'),
-    btnStart = document.getElementById('btn');
+var score = 0, //กำหนดตัวแปร score = 0
+    highScore = 0,  //กำหนดตัวแปร highscore = 0
+    time = 30,  //กำหนดตัวแปร time = 30
+    timer;  //กำหนดตัวแปร timer
+var IsPlaying = false; //กำหนดตัวแปร IsPlaying = false
+var timeBoard = document.getElementById('time'), //นำ id time มาแทนลงในตัวแปร timeBoard
+    scoreBoard = document.getElementById('score'), //นำ id score มาแทนลงในตัวแปร scoreBoard
+    btnStart = document.getElementById('btn'); //นำ id btn มาแทนลงในตัวแปร btnStart
 
 
-function fallDown(apple) {
-    if (!(IsPlaying && apple instanceof HTMLElement)) {
-        return;
+function fallDown(apple) { //สร้าง function fallDown รับค่าพารามิเตอร์ที่มีชื่อว่า apple เข้ามา
+    //เป็นการเช็คว่าถ้ายังไม่มีการกดปุ่ม start จะไม่สามารถเริ่มเกมส์ได้
+    if (!(IsPlaying && apple instanceof HTMLElement)) { 
+        return; //ไม่มีการกดให้ return ออกไป
     }
-    apple.setAttribute('data-top', apple.style.top);
-    apple.style.top = "380px";
-    score = score + 5;
-    renderScore();
-    hideFallenApple(apple);
+    apple.setAttribute('data-top', apple.style.top); //กำหนด Attribute ที่มีชื่อว่า data-top ของ apple ให้มีค่าเป็น apple.style.top
+    apple.style.top = "380px"; //ทำการเซตให้ apple ลงมาที่ 380px
+    score = score + 5; //กำหนดให้ score = score + 5 เพิ่มค่า score ต่อลูก ลูกละ 5 คะแนน
+    renderScore(); //เรียกฟังก์ชัน renderScore
+    hideFallenApple(apple); //เรียกฟังก์ชัน  hideFallenApple รับค่าพารามิเตอร์ที่มีชื่อว่า apple เข้าไป
 }
 
 
-function hideFallenApple(apple) {
-    setTimeout(function () {
-        apple.style.display = 'none';
-        restoreFallenApple(apple);
-    }, 501);
+function hideFallenApple(apple) { //สร้าง function hideFallenApple รับค่าพารามิเตอร์ที่มีชื่อว่า apple เข้ามา
+    setTimeout(function () { //เรียกใช้ฟังก์ชันหลังจากหมดเวลาหน่วงที่กำหนด
+        apple.style.display = 'none'; //ทำการลบ apple ออก โดยกำหนดให้  apple.style.display = none
+        restoreFallenApple(apple); //เรียกใช้ฟังก์ชัน restoreFallenApple รับค่าพารามิเตอร์ที่มีชื่อว่า apple เข้าไป
+    }, 500); //มีการหน่วงเวลา 0.5 วินาที
 }
 
 
-function restoreFallenApple(apple) {
-    apple.style.top = apple.getAttribute('data-top');
-    setTimeout(function () {
-        apple.style.display = 'inline-block';
-    }, 501);
+function restoreFallenApple(apple) { //สร้าง function restoreFallenApple รับค่าพารามิเตอร์ที่มีชื่อว่า apple เข้ามา
+    apple.style.top = apple.getAttribute('data-top'); //กำหนดให้ apple.style.top = นำ Attribute คือ data-top ใน apple
+    setTimeout(function () { //เรียกใช้ฟังก์ชันหลังจากหมดเวลาหน่วงที่กำหนด
+        apple.style.display = 'inline-block'; //ทำการเพิ่ม apple โดยกำหนดให้  apple.style.display = inline-block
+    }, 500); //มีการหน่วงเวลา 0.5 วินาที
 }
 
-function renderScore() {
-    scoreBoard.innerText = score;
-    if (score > highScore) {
-        highScore = score;
-        document.getElementById('high').innerText = highScore;
-    }
-}
-function startGame() {
-    btnStart.disabled = "disabled";
-    IsPlaying = true;
-    renderScore();
-    timer = setInterval(countDown, 1000);
-}
-function countDown() {
-    time = time - 1;
-    timeBoard.innerText = time;
-    if (time == 0) {
-        clearInterval(timer);
-        endGame();
+function renderScore() {  //สร้าง function renderScore
+    scoreBoard.innerText = score; //ให้ตัวแปร scoreBoard แสดงข้อความ score
+    if (score > highScore) { //ถ้า score มีค่ามากกว่า highscore 
+        highScore = score; //กำหนดให้ highScore = score
+        document.getElementById('high').innerText = highScore; //ทำการเข้าถึง id high และทำการเปลี่ยนค่าเป็น highScore
     }
 }
-function endGame() {
-    IsPlaying = false;
-    alert("Your score is " + score);
-    score = 0;
-    time = 30;
-    btnStart.removeAttribute('disabled');
+function startGame() {  //สร้าง function startGame 
+    btnStart.disabled = "disabled"; //กำหนดให้ปุ่มใช้งานไม่ได้เมื่อเริ่มเกมส์
+    IsPlaying = true;//ให้ตัวแปร IsPlaying = true 
+    renderScore(); //เรียกฟังก์ชัน renderScore
+    timer = setInterval(countDown, 1000); //กำหนดให้ตัวแปร time = เรียกใช้ฟังก์ชัน countDown ทุกๆ 1 วินาที
+}
+function countDown() {  //สร้าง function countDown
+    time = time - 1; //กำหนดให้ time = time -  1
+    timeBoard.innerText = time; //ให้ตัวแปร timeBoard แสดงข้อความ time
+    if (time == 0) { //ถ้า time = 0 
+        clearInterval(timer);  //ทำการรีค่าของ timer
+        endGame(); //เรียกฟังก์ชัน endGame 
+    }
+}
+function endGame() {  //สร้าง function endGame  
+    IsPlaying = false; //ให้ตัวแปร IsPlaying = false 
+    alert("Your score is " + score); //ทำการแสดงข้อความว่า Your score is ตามด้วยค่า score
+    score = 0;  //ให้ตัวแปร score = 0 
+    time = 30;  //ให้ตัวแปร time = 30 
+    btnStart.removeAttribute('disabled'); //กำหนดให้ตัวแปร btnStart ลบ Attribute disabled 
 }
